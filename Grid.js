544d6b276e4e281c47da8f8e3994fcb6d8286821
Isauro Ruvalcaba <isauroruvalcaba@sbcglobal.net>
2024-06-19 22:14:07 -0700

    added a method displayGrid() that write the emojis to the console with out displaying it as arrays.

--- Grid.js
+++ Grid.js
@@ -19,7 +19,19 @@ class Grid {
     // goal - top right
     this.grid[0][width - 1] = "⭐️";
 
-    console.log(this.grid);
+    // console.log(this.grid);
+    this.displayGrid();
+  }
+
+  displayGrid() {
+    for (let row = 0; row < this.height; row++) {
+      for (let col = 0; col < this.width; col++) {
+        // console.log(this.grid[row][col]);
+        process.stdout.write(this.grid[row][col]);
+        process.stdout.write("\t");
+      }
+      process.stdout.write("\n");
+    }
   }
 }
 
